3893dfbee5043820949fba6ae83db6a1058c7017
Zaine Mendes <zainemendes@hotmail.com>
2024-06-28 22:27:05 -0300

    Creating and placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -36,4 +36,12 @@ public class Board {
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
 	}
+	
+	//método feito fora da aula - REVISAR
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
+	
+	
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 //classe contendo as regras do jogo de xadrez
@@ -9,6 +12,7 @@ public class ChessMatch {
 	public ChessMatch() {
 		//é aqui se inicia o processo da criação do xadrez, definindo que a partida possue um tabuleiro e que esse tabuleiro possui 8/8
 		board = new Board(8, 8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces() {
@@ -19,5 +23,11 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.white), new Position(2, 1));
+		board.placePiece(new King(board, Color.black), new Position(0, 4));
+		board.placePiece(new King(board, Color.white), new Position(7, 4));
 	}
 }
